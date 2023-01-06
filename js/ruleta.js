fc0e8f5fc561b21a0ee7c9e4c3325be4db8548cc
toniferra-dev <info@toniferra.com>
2023-01-06 19:04:54 +0100

    retoque finales de la versión 1

--- js/ruleta.js
+++ js/ruleta.js
@@ -1,6 +1,6 @@
 // Variables
 const miFormulario = document.querySelector("#mi-formulario");
-const preguntaH4 = document.querySelector("#pregunta");
+// const preguntaH4 = document.querySelector("#pregunta");
 const respuestaH5 = document.querySelector("#respuesta");
 const info = document.querySelector("#nota");
 
@@ -20,11 +20,31 @@ miFormulario.addEventListener("submit", function (evento) {
     // RESPUESTAS aleatorias
     // Crea una matriz de respuestas posibles
     const respuestas = [
-        "Sí",
-        "No",
-        "Tal vez",
-        "Es posible",
-        "Probablemente no"
+        "Ellos se lo pierden",
+        "Me da pereza",
+        "No va conmigo",
+        "Ya veré que se puede hacer",
+        "Es que genera envidias",
+        "No nos da la vida",
+        "No tengo tiempo",
+        "Tengo que hablarlo con Penélope",
+        "No me gusta repetir juegos",
+        "Tengo el coche averiado",
+        "Penélope no abre la cartera",
+        "No es nuestro target",
+        "🤷🏻‍♂️",
+        "😂",
+        "El manual es un infierno",
+        "Tiene erratas",
+        "No lo hace FFG",
+        "Me duele la espalda",
+        "Te escribo pro privado",
+        "Estoy castigado",
+        "Eso es un euro",
+        "Tengo a la niña",
+        "Bueno, ya son las 21h...",
+        "Mi madre no me deja",
+        "Eso es fanmade"
     ];
 
     // Esta función devuelve una respuesta aleatoria de la matriz
@@ -33,7 +53,7 @@ miFormulario.addEventListener("submit", function (evento) {
         return respuestas[indiceAleatorio];
     }
     // insertar los valores al h4 y h5
-    preguntaH4.innerHTML = valor;
+    // preguntaH4.innerHTML = valor;
     respuestaH5.innerHTML = obtenerRespuesta();
 
     // limpiar input
